5511aa532fe8946191b5fb6fe73da8c9bbbd5b92
Schuster-Soliven <77858227+Schuster-Soliven@users.noreply.github.com>
2023-07-20 18:31:42 -0700

    version2
    main fixed

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ print('As the user, you are hypothetically given $100,000 to use on the stock ma
 print('Which ETF would you like to examine?')
 
 user_input = 'NULL'
-while (user_input != 'U' or user_input != 'D') :
-    print('FNGD (D) or FNGU (U)?')
+while (user_input != 'U' and user_input != 'D') :
     user_input = input('FNGD (D) or FNGU (U)?')
 
 print('We will download targeted data from Yahoo Finance starting from 2020 January 1')
@@ -19,4 +18,3 @@ if (user_input == 'D') :
 else:
     download_and_save("FNGU", start="2020-01-01", end=yesterday_str)
 
-    
