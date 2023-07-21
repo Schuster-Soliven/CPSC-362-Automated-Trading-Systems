034453d52130df82fe32aeeb0a2569dffa7b708b
Schuster-Soliven <77858227+Schuster-Soliven@users.noreply.github.com>
2023-07-20 18:46:31 -0700

    test1_ver3_final

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 
 from download_data import *
 
-dataFile = 'FNGU_data.json'
+dataFile = 'FNGD_data.json'
 
 
 import os
@@ -23,18 +23,13 @@ class Test01_FileContentsIsList(unittest.TestCase):
         #change prints to .assertEqual()
         download_and_save("FNGD", start="2020-01-01", end=yesterday_str)
         try:
-            print('1')
             with open(dataFile) as f:
                 # call download function and dump into f
-                print('2')
                 data = json.load(f)
                 # load dataFile contents into data
-                print(type(data))
                 self.assertEqual(list, type(data))
                 # making sure data is type list
                 # conversely 'list' to <class 'list'>
-    
-        # leave for now
         except FileNotFoundError:
             print("FAIL: File not found")
 
@@ -43,7 +38,7 @@ class Test01_FileContentsIsList(unittest.TestCase):
             
         remove_file(dataFile)
             
-            
+'''       
 class Test02_AccurateDayCount(unittest.TestCase):
     def test_list_int(self):
         """
@@ -65,7 +60,7 @@ class Test03_MatchDay(unittest.TestCase):
         with open(dataFile) as f:
             data = json.load(f)
         self.assertEqual(data[0], {"Date":"2020-01-02T00:00:00.000","Open":62.1199989319,"High":65.2099990845,"Low":62.0800018311,"Close":65.2099990845,"Volume":155900})
-            
+        '''    
 if __name__ == '__main__':
     with open('test.txt', "w") as f:
         runner = unittest.TextTestRunner(f)
