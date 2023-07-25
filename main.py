7ea62c56bcc7c3c158390345c06b26c24649cbc8
Schuster-Soliven <77858227+Schuster-Soliven@users.noreply.github.com>
2023-07-24 19:38:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from download_data import *
 print('Welcome to HARE (Highly Advanced Rapid Exchange).')
 print('As the user, you are hypothetically given $100,000 to use on the stock market.')
 
+# Start of loop
 # ETF choice
 print('Which ETF would you like to examine?')
 
@@ -18,4 +19,22 @@ if (user_input == 'D') :
     download_and_save("FNGD", start="2020-01-01", end=yesterday_str)
 else:
     download_and_save("FNGU", start="2020-01-01", end=yesterday_str)
+print('Download successful.')
 
+
+# Select Range
+while((int != type(user_input) or user_input <= 1012020)):
+    # change if to be acceptable
+    user_input = input('Please enter a start date (MMDDYYYY) of which will contain ETF information until the end date of (yesterday). Note that the maximum range is 01012020: ')
+print('Date is within expected range. Data will now range from ' + user_input + ' to yesterday\'s date.')
+
+# User prompt to choose strategy
+print('The user is automatically given to have $100,000 before strategy simulation.')
+while(user_input != 'B' or user_input != 'M' or user_input != 'T'):
+    print('Which type of strategy would the user like to use?')
+    user_input = input('Bollinger-Band-Bounce (B)\nMoving-Average (M)\nTrust the system (T)\n')
+print('Strategy chosen, returns are being calculated.')
+
+
+# End Program
+print('Program will be terminated, we thank you for using HARE.')
