09f73c8b950a246867b760955eb1db120d19432f
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2023-07-26 20:10:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
-from download_data import *
 from yf_api import *
 
-# Introduction Prompt
 print('Welcome to HARE (Highly Advanced Rapid Exchange).')
 print('As the user, you are hypothetically given $100,000 to use on the stock market.')
 
-# Start of loop
-# ETF choice
 print('Which ETF would you like to examine?')
 
-user_input = 'NULL'
-while (user_input != 'U' and user_input != 'D') :
-    etf = input('FNGD (D) or FNGU (U)?')
+etf_map = {'D': 'FNGD', 'U': 'FNGU'}
+
+user_input = None
+while user_input not in etf_map:
+    user_input = input('FNGD (D) or FNGU (U)?')
+etf = etf_map[user_input]
 
 print('We will download targeted data from Yahoo Finance starting from 2020 January 1')
 
 # Download data!
-date = 'NULL'
-if (date == 'D') : 
-    # download_and_save("FNGD", start="2020-01-01", end=yesterday_str)
-    choose_date('FNGD')
-else:
-   choose_date()
+start_date = '01012020'
+choose_date(etf, start_date)
 print('Download successful.')
 
+# Ask user for start date
+user_input = None
+while (not isinstance(user_input, int) or user_input <= 1012020):
+    user_input = int(input('Please enter a start date (MMDDYYYY) of which will contain ETF information until the end date of (yesterday). Note that the maximum range is 01012020: '))
+print(f'Date is within expected range. Data will now range from {user_input} to yesterday\'s date.')
 
-# Select Range
-while((int != type(user_input) or user_input <= 1012020)):
-    # change if to be acceptable
-    user_input = input('Please enter a start date (MMDDYYYY) of which will contain ETF information until the end date of (yesterday). Note that the maximum range is 01012020: ')
-print('Date is within expected range. Data will now range from ' + user_input + ' to yesterday\'s date.')
+# Ask user for strategy
+strategy_map = {'B': 'Bollinger-Band-Bounce', 'M': 'Moving-Average', 'T': 'Trust the system'}
+user_input = None
+while user_input not in strategy_map:
+    user_input = input('Which type of strategy would the user like to use? Bollinger-Band-Bounce (B)\nMoving-Average (M)\nTrust the system (T)\n')
+strategy = strategy_map[user_input]
 
-# User prompt to choose strategy
-print('The user is automatically given to have $100,000 before strategy simulation.')
-while(user_input != 'B' or user_input != 'M' or user_input != 'T'):
-    print('Which type of strategy would the user like to use?')
-    user_input = input('Bollinger-Band-Bounce (B)\nMoving-Average (M)\nTrust the system (T)\n')
 print('Strategy chosen, returns are being calculated.')
 
-# backtest
-# returns are calculated
-# user another strategy?
-# try program again?
+# Here, you would add your logic for backtesting and calculating returns...
 
-# End Program
 print('Program will be terminated, we thank you for using HARE.')
