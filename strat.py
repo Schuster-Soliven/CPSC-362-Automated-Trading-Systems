c6a1e1a79acbe31885ea3857c358c10aaff89ee7
Schuster-Soliven <77858227+Schuster-Soliven@users.noreply.github.com>
2023-07-27 14:52:22 -0700

    Strategies (#14)
    * Created branch for strategies module
    
    * Update strat.py
    
    * Bollinger Band Bounce
    
    Now returns list of calls to buy, sell, or do nothing by day
    
    * MA added
    
    ---------
    
    Co-authored-by: Alisa! <majarovalisa@gmail.com>

--- strat.py
+++ strat.py
@@ -28,15 +28,56 @@ def BandBounce(file, window=20):
     #call backtesting module
     #print(callist)
 
+# use 20 days as data points
+def MovAvg(ticker):
+    # iter over dictionary taking close data
+    # check previous data point and next data point. If next data point is under or below the average, buy or sell
+    
+    #should print all close indices
+    for x in ticker['Close']:
+        print(x)
 
+    counter = 20
+    i = 0
+    sum = 0
+    average = 0
+    average_list = []
+    for x in ticker['Close']:
+        if(i < 20):
+            i+=1
+            average_list.append(x)
+        else:
+            break
+    i = 0
+ 
+    # should iterate starting from 0, and summate 20 indices and calculate avg 
+    while(i < len(ticker['Close'])):
+        if (counter < 20):
+            # sum of 20 
+            sum += ticker['Close'][i]
+        elif (counter == 20):
+            # average it
+            average = sum/20
+            average_list.append(average)
+        else:
+            #reset sum
+            sum = 0
+    
+    #now iterate through ticker list of close and compare average_list to choose when to buy or sell or nothing
+    while(i < len(ticker['Close'])):
+        
+        print('')
 
-
-#def MovAvg(file):
-    #print('average')
-
-
-#def BandBounceTest():
-    #maybe
-
-
+    '''
+    read file
+    x is average of 20 days of close price
+    if b < x < a:
+      sell
+    if b > x > a:
+      buy
+    else:
+      do nothing
+    call backtest
+    '''
+    print('average')
 BandBounce(choose_date())
