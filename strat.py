1edcf5cede85d37a29bf3394d4ae7f8ead927533
Alisa! <majarovalisa@gmail.com>
2023-07-26 19:56:04 -0700

    Bollinger Band Bounce
    Now returns list of calls to buy, sell, or do nothing by day

--- strat.py
+++ strat.py
@@ -1,23 +1,42 @@
 #module contains both Bollinger Band Bounce and Moving Average Crossover
 from yf_api import *
 
-def BandBounce(file):
-    print('standard dev')
-    # TO DO
-    # pull 'close' data
-    # calculate middle, upper, and lower bands
+
+def BandBounce(file, window=20):
+    #pull 'close' data
+    file = file['Close']
+
+    #calculate middle, upper, and lower bands
     #ASSUMING THAT    window = 20    band (std dev) = 2
     #when upper & lower bands squeeze together, good sign to buy/sell cuz will usually expand after
-    mean = file.rolling(window=20).mean()
-    stdDev = file.rolling(window=20).mean()
+    mean = file.rolling(window).mean()
+    stdDev = file.rolling(window).std()
     uBand = mean + 2 * stdDev
     lBand = mean - 2 * stdDev
+    
+    #create list of calls to buy / sell / do nothing
+    callist = []
+    for n in range(len(file)):
+        #print(file[n])
+        if file[n] > uBand[n]:     #buy (-1)
+            callist.append(-1)
+        elif file[n] < lBand[n]:   #sell (1)
+            callist.append(1)
+        else:                   #do nothing (0)
+            callist.append(0)
 
-    #call backtest
-
-def MovAvg(file):
-    print('average')
+    #call backtesting module
+    #print(callist)
 
 
-def BandBounceTest():
+
+
+#def MovAvg(file):
+    #print('average')
+
+
+#def BandBounceTest():
     #maybe
+
+
+BandBounce(choose_date())
