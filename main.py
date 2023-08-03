2d2849fb37300b4ba78c962e5a77efd3d5a41418
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2023-08-02 20:45:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 This file: main.py
 Description: User interface
 '''
-from yf_api import *
-from strat import *
+from yf_api import yf_api
+from strat import give_date, strat
 from displayGraph import *
+from backtesting import Backtesting
 import os
+
+#Create an instance of the class
+yf_instance = yf_api()
+
+# Now you can use the choose_date method
+data = yf_instance.choose_date()
 
 def remove_file(filename):
     if os.path.exists(filename):
@@ -30,7 +37,7 @@ user_input = '00000000'
 while (int(user_input[0:2]) > 12 or int(user_input[2:4]) > 31 or int(user_input[4:8]) < 2020):
     user_input = input('Please enter a start date (MMDDYYYY) of which will contain ETF information until the end date of (yesterday). Note that the maximum range is 01012020: ')
 start_date = user_input
-y = choose_date(etf, start_date)
+y = yf_instance.choose_date(etf, start_date)
 print(f'Date is within expected range. Data will now range from {start_date} to yesterday\'s date.')
 
 # add graph here
@@ -42,18 +49,32 @@ strategy_map = {'B': 'Bollinger-Band-Bounce', 'M': 'Moving-Average', 'T': 'Trust
 user_input = None
 while user_input not in strategy_map:
     user_input = input('Which type of strategy would the user like to use? Bollinger-Band-Bounce (B)\nMoving-Average (M)\nTrust the system (T)\n')
+# Get ETF data using the choose_date function
+etf_data = yf_instance.choose_date(etf, start_date)
 if user_input == 'B' or user_input == 'Bollinger-Band-Bounce':
     d = give_date(y, strat.BandBounce(y))
     for x in d.items():
         print(x)
+        # Initialize and backtest the BandBounce strategy
+    band_bounce_backtesting = Backtesting('BandBounce', etf_data)
+    total_return_dollars, total_return_percentage = band_bounce_backtesting.backtest()
+    print('Strategy chosen, returns are being calculated.')
+    print("BandBounce Strategy Results:")
+    print("Total Return in Dollars:  $", total_return_dollars)
+    print("Total Return in Percentage:", total_return_percentage, "%")
 elif user_input == 'M' or user_input == 'Moving-Average':
     d = give_date(y, strat.MovAvg(y))
     for x in d.items():
         print(x)
+            # Initialize and backtest the MovAvg strategy
+    mov_avg_backtesting = Backtesting('MovAvg', etf_data)
+    total_return_dollars, total_return_percentage = mov_avg_backtesting.backtest()
+    print('Strategy chosen, returns are being calculated.')
+    print("Moving Average Strategy Results:")
+    print("Total Return in Dollars:  $", total_return_dollars)
+    print("Total Return in Percentage:", total_return_percentage, "%")
 else:
     print('Trust the System WIP')
-
-print('Strategy chosen, returns are being calculated.')
 
 # Here, you would add your logic for backtesting and calculating returns...
 
